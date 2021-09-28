27ff085477db13b9d7674b1914a7fdd80c52f4ce
shushengfuqin <ruocjia@gmail.com>
2021-09-28 19:00:55 -0400

    azure function get_pic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const got = await fetch('/api/get_pic')
+      const text = await got.text()
+      console.log(text);
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>
 }
 
 export default App;
